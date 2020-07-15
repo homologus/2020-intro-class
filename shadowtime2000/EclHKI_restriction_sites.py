12fab5292132cf58f6693dafe74afb684cc7dfe5
shadowtime2000 <ahuja.pulkit@outlook.com>
2020-07-15 21:36:01 +0000

    Created Fragmentation for EclHKI Restriction Sites

---
+++ shadowtime2000/EclHKI_restriction_sites.py
@@ -0,0 +1,27 @@
+import sys
+
+restriction_site1 = "GAC"
+restriction_site2 = "GTC"
+
+restriction_site_reverse1 = "CTG"
+restriction_site_reverse2 = "CAG"
+
+genome_file = sys.argv[1]
+
+dna_sequence = ""
+
+with open(genome_file, "r") as f:
+	for line in f.readlines():
+		if not line[0] == ">":
+			dna_sequence = dna_sequence + line
+dna_sequence = dna_sequence.replace("\n", "")
+
+for i in range(0, len(dna_sequence), 11):
+	if (dna_sequence[i:i+3] == restriction_site1 and dna_sequence[i+8:i+12] == restriction_site2):
+		dna_sequence.replace(dna_sequence[i:i+12], "*")
+
+fragments = dna_sequence.split("*")
+
+for i in range(len(fragments)):
+	print("Fragment ", i, " Length: ", len(fragments[i]))
+
